e8bbaaaee032ad6871f0224d5718bb61af53e3da
Dan Nollette and safiy cham <safiy@>
2017-08-14 10:39:54 -0700

    add loops to dry the code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-//   $("h1").click(function() {
-//     alert("This is a header.");
-//   });
-//
-//   $("p").click(function() {
-//     alert("This is a paragraph.");
-//   });
-//
-//   $("img").click(function() {
-//     alert("This is an image.");
-//   });
+  var tags = ["h1", "p", "img"];
+  tags.forEach(function(tag){
+    $(tag).click(function() {
+      alert("This is a " + tag + " tag.")
+    });
+  });
+
+ //  $("h1").click(function() {
+ //    alert("This is a header.");
+ //  });
+ //
+ //  $("p").click(function() {
+ //    alert("This is a paragraph.");
+ //  });
+ //
+ //  $("img").click(function() {
+ //    alert("This is an image.");
+ //  });
  });
 
 $(function() {
